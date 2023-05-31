903c6f299e2ebe8263673682ada9a1acad51e6a2
George Alvarez <georgealvarez@macbook-pro.mynetworksettings.com>
2023-05-31 16:15:03 -0400

    code

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import './App.css';
 import {puppyList} from './data.js';
 
 function App() {
-  console.log("puppyList: ", puppyList);
+  console.log( puppyList);
   const [puppies, setPuppies] = useState(puppyList);
-  return (
+  const [featPupId, setFeatPupId] = useState(null);
+  
+    function handleClick() {
+      
+    }
+  
+    return (
       <div>
-        { 
-   puppies.map((puppy) => {
-     return <p>{puppy.name}</p>
-   })
-}
-     
-</div>
-  )
-}
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={handleClick} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
+    );
+  }  
+
 
 
 export default App
