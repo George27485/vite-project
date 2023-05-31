c2fd6a5d7ced3df63246e51fea6cf7d7037a90f4
George Alvarez <georgealvarez@macbook-pro.mynetworksettings.com>
2023-05-31 14:45:15 -0400

    added puppyList

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-
-import './App.css'
+import { useState } from 'react'
+import './App.css';
+import {puppyList} from './data.js';
 
 function App() {
-  
-
+  console.log("puppyList: ", puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
   return (
       <div>
-        
-      </div>
-      
+        { 
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
+     
+</div>
   )
 }
+
 
 export default App
