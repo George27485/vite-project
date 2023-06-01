9a3470031eca1392ecd41d77ae5844c10bca1334
George Alvarez <georgealvarez@macbook-pro.mynetworksettings.com>
2023-06-01 09:51:31 -0400

    code

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,33 @@ function App() {
   const [featPupId, setFeatPupId] = useState(null);
    
     
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
 
-
-  function clickHandle(){
-
-  
-
-    }
     return (
       <div>
         {puppies.map((puppy) => {
           return (
-            <p onClick={()=>{console.log("puppy id: ", puppy.id)}} key ={puppy.id}>
+            <p onClick={()=>{ setFeatPupId(puppy.id)}} key ={puppy.id}>
               {puppy.name}
             </p>
           );
           
         })}
+        <p>{ featPupId }</p> 
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
+      
     );
+   
   
       }
       
